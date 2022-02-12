b0bd1271dc22b2cfa5c03598bdf2d471b7f2ad31
your_username <your_email_address@example.com>
2022-02-12 15:24:56 +0500

    Pull

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
-
-  ngOnInit() {
-  }
-  ngOnDestroy() {
-  }
+export class LoginComponent{
+  loginForm= new FormGroup({
+    loginemail: new FormControl("", [Validators.required, Validators.email]),
+    loginpassword: new FormControl("", [Validators.required, Validators.minLength(8)]),
+  })
+  get loginemail(){
+   return this.loginForm.get('loginemail')
+ }
+ get loginpassword(){
+   return this.loginForm.get('loginpassword')
+ }
+ 
 
 }
